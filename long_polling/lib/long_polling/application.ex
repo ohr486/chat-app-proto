a5887428dad05ff166d6fdd236232a2119be0548
ohara_tsunenori <tsunenori.ohara@drecom.co.jp>
2017-01-14 18:03:04 +0900

    add lp base code

--- long_polling/lib/long_polling/application.ex
+++ long_polling/lib/long_polling/application.ex
@@ -1,6 +1,4 @@
 defmodule LongPolling.Application do
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,15 +6,26 @@ defmodule LongPolling.Application do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    # Define workers and child supervisors to be supervised
     children = [
-      # Starts a worker by calling: LongPolling.Worker.start_link(arg1, arg2, arg3)
-      # worker(LongPolling.Worker, [arg1, arg2, arg3]),
+      worker(__MODULE__, [], function: :run)
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: LongPolling.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  def run do
+    routes = [
+      {"/", LongPolling.Handler, []},
+      #{"/poll", LongPolling.LpEventHandler, []},
+      {"/static/[...]", :cowboy_static, {:priv_dir, :long_polling, "static_files"}}
+    ]
+
+    dispatch = :cowboy_router.compile([{:_, routes}])
+
+    opts = [{:port, 4000}]
+    env = %{dispatch: dispatch}
+
+    {:ok, _pid} = :cowboy.start_clear(:http, 100, opts, %{env: env})
   end
 end
--- long_polling/mix.exs
+++ long_polling/mix.exs
@@ -11,11 +11,13 @@ defmodule LongPolling.Mixfile do
   end
 
   def application do
-    [extra_applications: [:logger],
+    [extra_applications: [:logger, :cowboy],
      mod: {LongPolling.Application, []}]
   end
 
   defp deps do
-    []
+    [
+      {:cowboy, git: "https://github.com/ninenines/cowboy.git", tag: "2.0.0-pre.5"}
+    ]
   end
 end
